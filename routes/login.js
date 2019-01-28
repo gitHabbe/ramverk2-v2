16784b95f1e88f2e02f9375b24de1d4e52323f5c
Niklas Hallberg <91habbe@gmail.com>
2019-01-28 13:53:50 +0100

    added working async calls

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,26 @@
-var express = require('express');
-var router  = express.Router();
+var express     = require('express');
+var router      = express.Router();
+const bcrypt    = require('bcrypt');
+const sqlite3   = require('sqlite3').verbose();
+const db        = new sqlite3.Database('./db/texts.sqlite');
 
-router.post('/', function(req, res, next) {
-    console.log("Hey");
+router.post('/', async function(req, res, next) {
+    const { email, password } = req.body;
+
+    const getUser = () => {
+        return new Promise((resolve, reject) => {
+            db.all(
+                "SELECT * FROM users WHERE email = ?",
+                [ email ], (err, data) => {
+                if(err) reject("Read error: " + err.message)
+                else {
+                    resolve(data[0])
+                }
+            })
+        });
+    };
+    const user = await getUser();
+    const isCorrectPass = await bcrypt.compare(password, user.password);
     res.send("ok");
 });
 
