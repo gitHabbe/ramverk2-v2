5da285774c2a339ab22101fb9a500d9421dda554
Niklas Hallberg <91habbe@gmail.com>
2019-01-28 17:05:04 +0100

    updated /report/kmom to read DB instead of .txt files

--- routes/report.js
+++ routes/report.js
@@ -1,14 +1,38 @@
-var express = require('express');
-var router  = express.Router();
-var fs      = require('fs');
+var express     = require('express');
+var router      = express.Router();
+var fs          = require('fs');
+const sqlite3   = require('sqlite3').verbose();
+const db        = new sqlite3.Database('./db/texts.sqlite');
 
-router.get('/kmom01', function(req, res, next) {
-    const text = fs.readFileSync("./reports/kmom01.txt", 'utf8');
+router.get('/kmom01', async (req, res) => {
+    const kmomData = await getReport("kmom01");
     const data = {
-        data: { text }
+        kmomData
     };
 
     res.json(data);
 });
+
+router.get('/kmom02', async (req, res) => {
+    const kmomData = await getReport("kmom02");
+    const data = {
+        kmomData
+    };
+
+    res.json(data);
+});
+
+async function getReport(kmom) {
+    return new Promise((resolve, reject) => {
+        db.all(
+            "SELECT * FROM reports WHERE kmom = ?",
+            [ kmom ], (err, data) => {
+            if(err) reject("Read error: " + err.message)
+            else {
+                resolve(data[0])
+            }
+        })
+    });
+};
 
 module.exports = router;
