f4004e97c399c5b0740fb5ca4fdedc818700d95e
Niklas Hallberg <91habbe@gmail.com>
2019-02-01 14:18:48 +0100

    addedmore testcases to reach 100% coverage

--- test/fakeapi/fakeapi.test.js
+++ test/fakeapi/fakeapi.test.js
@@ -8,10 +8,28 @@ test("Get home data", () => {
     expect(res.data).toBe(exp);
 });
 
+test("Get about data", () => {
+    const exp = 200;
+    const res = API.getGet("/about");
+    expect(res.status).toBe(exp);
+});
+
 test("Get route error", () => {
     const exp = 404;
     const res = API.getGet("asdf");
     expect(res.status).toBe(exp);
+});
+
+test("Get unposted report", () => {
+    const exp = "undefined";
+    const res = API.getGet("/reports");
+    expect(typeof res.reports[3]).toBe(exp);
+});
+
+test("Get reports data", () => {
+    const exp = "string";
+    const res = API.getGet("/reports");
+    expect(typeof res.reports[0].data).toBe(exp);
 });
 
 test("Post route error", () => {
@@ -23,19 +41,27 @@ test("Post route error", () => {
 });
 
 test("Post submit report", () => {
-    let exp = "This is a report";
+    const exp = "This is a report";
     const res = API.getPost("report", "This is a report");
     expect(res.reportInfo).toBe(exp);
 });
 
 test("Post submit login information", () => {
-    let exp = 201;
-    const res = API.getPost("report", "coolUsername");
+    const exp = 201;
+    const res = API.getPost("login", "coolUsername");
     expect(res.status).toBe(exp);
 });
 
 test("Post submit login error", () => {
-    let exp = 201;
-    const res = API.getPost("report");
+    const exp = 201;
+    const res = API.getPost("login");
     expect(res.status).not.toBe(exp);
+});
+
+test("Post unsupported route", () => {
+    let exp = "No data provided";
+    let res = API.getPost("/yahoo");
+    exp = 404;
+    res = API.getPost("/yahoo", "some data");
+    expect(res.status).toBe(exp);
 });
